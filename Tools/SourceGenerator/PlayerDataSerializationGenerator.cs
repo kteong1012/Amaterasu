7f93eab7015d607ef0d866d993ee39ff520b5fb5
Tianbao Lin <kteong1012@outlook.com>
2024-04-12 20:50:49 +0800

    [refactor] 整理Tools目录

---
+++ Tools/SourceGenerator/PlayerDataSerializationGenerator.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.CodeAnalysis;
+using System;
+
+namespace SourceGenerator
+{
+    [Generator(LanguageNames.CSharp)]
+    public class PlayerDataSerializationGenerator : ISourceGenerator
+    {
+        public void Initialize(GeneratorInitializationContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Execute(GeneratorExecutionContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
