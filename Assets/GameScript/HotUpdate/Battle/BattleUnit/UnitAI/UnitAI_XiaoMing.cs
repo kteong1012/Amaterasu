3b42710d95fdc1656950aa4098fde7d432a981f1
lintianbao <lintianbao@thedream.cc>
2024-04-23 15:42:16 +0800

    [revert] 还是不逃跑了

--- Assets/GameScript/HotUpdate/Battle/BattleUnit/UnitAI/UnitAI_XiaoMing.cs
+++ Assets/GameScript/HotUpdate/Battle/BattleUnit/UnitAI/UnitAI_XiaoMing.cs
@@ -27,15 +27,7 @@ namespace Game
             if (currentTarget != null)
             {
                 _currentTargetId = currentTarget.InstanceId;
-                // 血量低于50%时,逃跑
-                if (battleUnitController.GetStatsValue(NumericId.HP) < battleUnitController.GetStatsValue(NumericId.MaxHP) / 2)
-                {
-                    Escape(battleUnitController, currentTarget);
-                }
-                else
-                {
-                    ChaseOrAttack(battleUnitController, currentTarget, range);
-                }
+                ChaseOrAttack(battleUnitController, currentTarget, range);
             }
         }
 
@@ -50,20 +42,6 @@ namespace Game
                 var stopDistance = BattleCalculator.CalculateNavigationStopDistance(selfUnit, targetUnit, range);
                 selfUnit.ChaseUnit(targetUnit, stopDistance);
             }
-        }
-
-        public void Escape(BattleUnitController selfUnit, UnitController targetUnit)
-        {
-            // move distance equals to move speed * actitv
-            var moveSpeed = selfUnit.GetStatsValue(NumericId.MoveSPD);
-            var ACTITV = selfUnit.GetStatsValue(NumericId.ACTITV);
-            var ACTSPD = selfUnit.GetStatsValue(NumericId.ACTSPD);
-            var finalACTITV = BattleCalculator.CalculateFinalACTITV(ACTITV, ACTSPD);
-            var moveDistance = moveSpeed * finalACTITV;
-
-            var direction = selfUnit.transform.position - targetUnit.transform.position;
-            var targetPosition = selfUnit.transform.position + direction.normalized * moveDistance;
-            selfUnit.MoveToPlace(targetPosition, NumberX1000.Zero);
         }
     }
 }
